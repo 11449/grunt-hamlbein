d198204ebe0da330f7b87b39d3b9d6d08bec1e49
Andreas Kürbis <ich@andreas-kuerbis.de>
2014-12-18 17:40:39 +0100

    [npm install] update async process

--- tasks/hamlbein.js
+++ tasks/hamlbein.js
@@ -8,34 +8,42 @@
 
 'use strict';
 
-module.exports = function(grunt) {
-  var async = grunt.util.async,
-      path  = require('path');
+var async = require('async'),
+    path  = require('path'),
+    spawn = require('win-spawn');
 
+module.exports = function(grunt) {
   grunt.registerMultiTask('hamlbein', 'compile haml files with ruby haml wrapper', function() {
-    var args         = [],
-        doneCallback = this.async(),
-        options      = this.options({
+    var args     = [],
+        bin      = 'hamlbein',
+        cb       = this.async(),
+        options  = this.options({
           context:             false,
           includePath:         false,
           layout:              false,
           rubyHamlBeinCommand: 'bin/hamlbein'
-        });
+        }),
+        cp,
+        src;
 
     grunt.verbose.writeflags(options, 'Options');
 
-    async.forEach(this.files, function(file, callback) {
-      var src = file.src.filter(function(filepath) {
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        return filepath;
-      });
+    bin = path.resolve(__dirname, '..', options.rubyHamlBeinCommand);
 
+    async.each(this.files, function(file, next){
+      src = file.src[0];
+
+      if (typeof src !== 'string') {
+        src = file.orig.src[0];
+      }
+
+      // check if file exists
+      if (!grunt.file.exists(src)) {
+        grunt.log.warn('Source file "' + src + '" not found.');
+        return next();
+      }
+
+      // prepare haml args
       if(options.layout) {
         args.push('--layout', options.layout);
       }
@@ -44,21 +52,28 @@ module.exports = function(grunt) {
         args.push('--context', JSON.stringify(options.context));
       }
 
-      args.push(path.resolve(process.cwd(), src[0]));
+      args.push(path.resolve(process.cwd(), src));
       args.push(path.resolve(process.cwd(), file.dest));
 
       grunt.verbose.debug(args.join(' '));
 
-      grunt.util.spawn({
-        cmd:  path.resolve(__dirname, '..', options.rubyHamlBeinCommand),
-        args: args,
-      }, function done() {
-        grunt.verbose.writeflags(arguments, 'arguments');
-        grunt.log.writeln('File "' + file.dest + '" created.');
+      // Make sure grunt creates the destination folders if they don't exist
+      if (!grunt.file.exists(file.dest)) {
+        grunt.file.write(file.dest, '');
+      }
 
-        doneCallback();
+      grunt.verbose.writeln('Command: ' + bin + ' ' + args.join(' '));
+
+      cp = spawn(bin, args, {stdio: 'inherit'});
+      cp.on('error', grunt.warn);
+      cp.on('close', function (code) {
+        if (code > 0) {
+          return grunt.warn('Exited with error code ' + code);
+        }
+
+        grunt.verbose.writeln('File ' + file.dest + ' created.');
+        next();
       });
-    }, doneCallback);
+    }, cb);
   });
-
 };
